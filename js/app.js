34d77ba44932871ec787ee75d996ce560f47cdf7
Charlotte Leedham <c.leedham18@gmail.com>
2017-09-18 14:33:38 +0100

    Player one in place

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 $(function(){
+	$('.rollButton').on('click', function() {
+		var dice = 1 + Math.floor(Math.random() * 6);
+		$('.displayDice').html(dice);
+
+	});
+	//places player in start position
+	$('li').eq(0).append(player1);
+	var start 
+	
+
 	
 });
