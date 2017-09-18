a5f46a605fe258250946a31e948a4573d88fd8da
Charlotte Leedham <c.leedham18@gmail.com>
2017-09-18 14:57:07 +0100

    player1 moves with dice

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 $(function(){
+	//places player in start position
+	var position = 1;
 	$('.rollButton').on('click', function() {
 		var dice = 1 + Math.floor(Math.random() * 6);
 		$('.displayDice').html(dice);
+		//moves player to position on board
+		position = position + dice;
+		$('li[value="'+position+'"]').append(player1);
 
 	});
-	//places player in start position
-	$('li').eq(0).append(player1);
-	var start 
 	
-
+	
+	
 	
 });
