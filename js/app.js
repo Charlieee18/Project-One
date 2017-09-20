d0e1a7f460749bd10ffbb61a7ed33b9a023613f0
Charlotte Leedham <c.leedham18@gmail.com>
2017-09-20 09:41:17 +0100

    Basic Snakes and Ladders working

--- js/app.js
+++ js/app.js
@@ -3,9 +3,8 @@ $(function(){
 	var p1position = 0;
 	var turn 
 	var alternate = true;
+	var player
 	var p2position = 0;
-	
-
 
 	$('.rollButton').on('click', function() {
 		var dice = 1 + Math.floor(Math.random() * 6);
@@ -13,14 +12,13 @@ $(function(){
 		//moves player to position on board
 		if (alternate === true) {
 			p1position = p1position + dice;
-			$('li[value="'+p1position+'"]').append(player1);
+			$('li[value="'+p1position+'"]').append(player1)
 			alternate = false;
 		} else{
 			p2position = p2position + dice;
-			$('li[value="'+p2position+'"]').append(player2);
+			$('li[value="'+p2position+'"]').append(player2)
 			alternate = true;
 		}
-	// console.log(clickValue)
 
 		if (p1position >= 49){
 			alert("Player 1 Wins!")
@@ -31,90 +29,147 @@ $(function(){
 
 		//snakes
 
-		if (p1position == 42 ){
+//		function snakes(){
+//			[42, 27]
+//			[48, 24]
+//			[45, 31]
+//			[26, 5]
+//			[15, 2]
+//		}
+//		function checkSnakes(){
+//		if position == snakes[0]{
+//			position = 1;
+//			setTimeout(function(){
+//			$('li[value="'+p1position+'"]').append(player);
+//			}, 1000);
+//		}
+//	};
+		if (p1position == 42 ) {
 			p1position = 27;
+			setTimeout(function(){
 			$('li[value="'+p1position+'"]').append(player1);
-		} else if (p2position == 42){
+			}, 1000);
+		} else if (p2position == 42) {
 			p2position = 27;
 			$('li[value="'+p2position+'"]').append(player2);
 		};
 
-		if (p1position == 48 ){
+		if (p1position == 48 ) {
 			p1position = 24;
-			$('li[value="'+p1position+'"]').append(player1);
-		} else if (p2position == 48){
-			p2position = 24;
+			setTimeout( function(){
+				$('li[value="'+p1position+'"]').append(player1);
+			}, 1000);
+		} else if (p2position == 48) {
+				p2position = 24;
+				setTimeout( function(){
 			$('li[value="'+p2position+'"]').append(player2);
+			}, 1000);	
 		};
 
-		if (p1position == 45 ){
+
+		if (p1position == 45) {
 			p1position = 31;
+			setTimeout( function(){
 			$('li[value="'+p1position+'"]').append(player1);
-		} else if (p2position == 45){
+			}, 1000);
+		} else if (p2position == 45) {
 			p2position = 31;
+			setTimeout( function(){
 			$('li[value="'+p2position+'"]').append(player2);
+			}, 1000);
 		};
 
-		if (p1position == 26 ){
+		if (p1position == 26 ) {
 			p1position = 5;
+			setTimeout( function(){
 			$('li[value="'+p1position+'"]').append(player1);
-		} else if (p2position == 26){
+			}, 1000);
+		} else if (p2position == 26) {
 			p2position = 5;
+			setTimeout( function(){
 			$('li[value="'+p2position+'"]').append(player2);
-		};
+		}, 1000);
+		
 
-		if (p1position == 15 ){
+		if (p1position == 15 ) {
 			p1position = 2;
+			setTimeout( function(){
 			$('li[value="'+p1position+'"]').append(player1);
-		} else if (p2position == 15){
+		}, 1000);
+		} else if (p2position == 15) {
+			setTimeout( function(){
 			p2position = 2;
 			$('li[value="'+p2position+'"]').append(player2);
+		}, 1000);
 		};
 
 		//ladders
 
 		if (p1position == 6 ){
+			setTimeout( function(){
 			p1position = 18;
 			$('li[value="'+p1position+'"]').append(player1);
+		}, 1000);
 		} else if (p2position == 6){
+			setTimeout( function(){
 			p2position = 18;
 			$('li[value="'+p2position+'"]').append(player2);
+		}, 1000);
 		};
 
 		if (p1position == 20 ){
 			p1position = 33;
+			setTimeout( function(){
 			$('li[value="'+p1position+'"]').append(player1);
+			}, 1000);
 		} else if (p2position == 20){
 			p2position = 33;
+			setTimeout( function(){
 			$('li[value="'+p2position+'"]').append(player2);
+		}, 1000);
 		};
 
 		if (p1position == 31 ){
 			p1position = 47;
+			setTimeout( function(){
 			$('li[value="'+p1position+'"]').append(player1);
+			}, 1000);
 		} else if (p2position == 31){
 			p2position = 47;
+			setTimeout( function(){
 			$('li[value="'+p2position+'"]').append(player2);
+		}, 1000);
 		};
 
 		if (p1position == 29 ){
 			p1position = 41;
+			setTimeout( function(){
 			$('li[value="'+p1position+'"]').append(player1);
+		}, 1000);
 		} else if (p2position == 29){
 			p2position = 41;
+			setTimeout( function(){
 			$('li[value="'+p2position+'"]').append(player2);
+		}, 1000);
 		};
 
 		if (p1position == 8 ){
 			p1position = 35;
+			setTimeout( function(){
 			$('li[value="'+p1position+'"]').append(player1);
+		}, 1000);
 		} else if (p2position == 8){
 			p2position = 35;
+			setTimeout( function(){
 			$('li[value="'+p2position+'"]').append(player2);
+		}, 1000);
 		};
+	
+}
 
 
 	});
+
 		//$( 'li[value=42]' ).appendTo( $( 'li[value=27]' ) );
 
 	
